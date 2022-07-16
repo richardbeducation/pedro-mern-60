efe4b8f665378e1a8ae4a3ce3b0852291e048b68
richardbeducation <richardbeducation@gmail.com>
2022-07-16 15:43:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,32 @@ import Axios from "axios";
 
 function App() {
   const [listOfUsers, setListOfUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     Axios.get("http://localhost:3001/getUsers").then((response) => {
       setListOfUsers(response.data);
     });
   }, []);
+
+  const createUser = () => {
+    Axios.post("http://localhost:3001/createUser", {
+      name,
+      age,
+      username,
+    }).then((response) => {
+      setListOfUsers([
+        ...listOfUsers,
+        {
+          name,
+          age,
+          username,
+        },
+      ]);
+    });
+  };
 
   return (
     <div className="App">
@@ -18,11 +38,36 @@ function App() {
           return (
             <div>
               <h1>Name: {user.name}</h1>
-              <h1>Age: {user.age}</h1>
-              <h1>Username: {user.username}</h1>
+              <h2>Age: {user.age}</h2>
+              <h2>Username: {user.username}</h2>
             </div>
           );
         })}
+      </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <button onClick={createUser}> Create User </button>
       </div>
     </div>
   );
